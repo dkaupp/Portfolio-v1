6167fd25c613cac034caa68af89da4f285e721d1
Arth K <mila2yoshi@gmail.com>
2021-02-25 14:07:29 -0800

    changed mail api provider

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,14 +3,8 @@ import sendMail from "../../utils/mailer";
 
 export default async (req, res) => {
   const { name, email, message } = req.body;
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL2,
-    subject: `Message from ${name} <${email}>`,
-    text: message,
-  };
   try {
-    sendMail(mailOptions);
+    sendMail(name, email, message);
     res.send("success");
   } catch (error) {
     console.log(error);
--- utils/mailer.js
+++ utils/mailer.js
@@ -1,22 +1,25 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
 
-let transporter = nodemailer.createTransport({
-  service: "Hotmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-const sendMail = (mailOptions) => {
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) return console.log(err);
+const sgMail = require("@sendgrid/mail");
 
-    return console.log(info);
-  });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendMail = (name, email, message) => {
+  const msg = {
+    to: process.env.EMAIL2, // Change to your recipient
+    from: process.env.EMAIL, // Change to your verified sender
+    subject: `${name} <${email}>`,
+    text: message,
+    html: `<p>${message}<p>`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 module.exports = sendMail;
