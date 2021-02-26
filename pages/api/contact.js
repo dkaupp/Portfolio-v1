0a3dbcc9125cf95558b4c65905e96525a91cba88
Arth K <mila2yoshi@gmail.com>
2021-02-25 17:51:17 -0800

    test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   const { name, email, message } = req.body;
   try {
     await sendMail(name, email, message);
-    await res.status(200).send("success");
+    res.status(200).send("success");
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
