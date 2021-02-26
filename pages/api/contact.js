2bdb3150384ce1cb4695f7d7bedfb605412f5213
Arth K <mila2yoshi@gmail.com>
2021-02-26 12:27:27 -0800

    sendgrid should now work properly on vercel

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,23 @@
-import sendMail from "../../utils/mailer";
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async (req, res) => {
   const { name, email, message } = req.body;
+
+  if (!name || !email || !message)
+    return res.status(400).send("Name , email and password are required.");
+
+  const msg = {
+    to: process.env.EMAIL2, // Change to your recipient
+    from: process.env.EMAIL, // Change to your verified sender
+    subject: `${name} <${email}>`,
+    text: message,
+    html: `<p>${message}<p>`,
+  };
+
   try {
-    await sendMail(name, email, message);
+    await sgMail.send(msg);
     res.status(200).send("success");
   } catch (error) {
     console.log(error);
--- utils/mailer.js
+++ utils/mailer.js
@@ -1,23 +1,3 @@
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const sendMail = (name, email, message) => {
-  const msg = {
-    to: process.env.EMAIL2, // Change to your recipient
-    from: process.env.EMAIL, // Change to your verified sender
-    subject: `${name} <${email}>`,
-    text: message,
-    html: `<p>${message}<p>`,
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+const sendMail = async (name, email, message) => {};
 
 module.exports = sendMail;
